1cdb4c974531889bba2dff3e7f7359f8f845bc46
Justin Merryman <justinmerryman@yahoo.com>
2021-03-21 09:58:32 -0500

    create map

---
+++ static/js/logic.js
@@ -0,0 +1,27 @@
+function createMap(earthQuakes){
+
+    let lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
+
+  let baseMaps = {
+      "Light Map": lightmap
+  };
+  let overlayMaps = {
+      'Earth Quakes': earthQuakes
+  };
+  // Create the map object with options
+  let map = L.map("map-id", {
+    center: [40.73, -74.0059],
+    zoom: 12,
+    layers: [lightmap, earthQuakes]
+  });
+
+// Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
+}
